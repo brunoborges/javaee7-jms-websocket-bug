8a73a3b59a9ba3874407ccbd11d8ecf9a4dd6187
Bruno Borges <bruno.borges@gmail.com>
2013-04-25 19:21:04 -0300

    chat funcionando, fila jms, e javascript

--- src/main/java/org/glassfish/javaee7sample/SampleWebSocket.java
+++ src/main/java/org/glassfish/javaee7sample/SampleWebSocket.java
@@ -4,16 +4,14 @@
  */
 package org.glassfish.javaee7sample;
 
-import java.io.IOException;
-import javax.jms.Queue;
+import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Resource;
 import javax.inject.Inject;
 import javax.jms.JMSContext;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.MessageListener;
+import javax.jms.Queue;
+import javax.validation.constraints.NotNull;
 import javax.websocket.OnClose;
 import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
@@ -24,57 +22,39 @@ import javax.websocket.server.ServerEndpoint;
  *
  * @author bruno
  */
-@ServerEndpoint("/websocket")
-public class SampleWebSocket implements MessageListener {
+@ServerEndpoint("/websocket/{nickname}")
+public class SampleWebSocket {
 
-    @Resource(name = "jms/myQueue")
-    private Queue myQueue;
-    @Inject
-    private JMSContext jmsContext;
-    private Session session;
+//    @Resource(mappedName = "jms/myQueue")
+//    private Queue myQueue;
+//    @Inject
+//    private JMSContext jmsContext;
+    private static HashSet<Session> sessions = new HashSet<>();
 
     @OnOpen
-    public void onOpen(final Session session) {
-        this.session = session;
-        Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket session opened");
-        try {
-            session.getBasicRemote().sendText("websocket session opened");
-        } catch (IOException ex) {
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void onOpen(@NotNull Session session) {
+        System.out.println("onOpen");
+        String nickname = session.getPathParameters().get("nickname");
+        Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket session opened for '{0}'", nickname);
+        sessions.add(session);
 
-        // create a queue consumer
-        jmsContext.createConsumer(myQueue).setMessageListener(this);
+//        System.out.println("queue exists? " + (myQueue != null));
     }
 
     @OnMessage
-    public void onMessage(String message, Session client) {
-        try {
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket message received: {0}", message);
-            client.getBasicRemote().sendText("echo: " + message);
-            jmsContext.createProducer().send(myQueue, message);
-        } catch (IOException ex) {
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.SEVERE, null, ex);
+    public void onMessage(@NotNull String message, @NotNull Session client) {
+        System.out.println("onMessage");
+        Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket session onMessage()");
+        Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket message received: {0}", message);
+        for (Session s : sessions) {
+            s.getAsyncRemote().sendText(client.getPathParameters().get("nickname") + "> " + message);
         }
     }
 
     @OnClose
-    public void onClose() {
-        try {
-            this.session.close();
-            this.session = null;
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket session closed");
-        } catch (IOException ex) {
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    @Override
-    public void onMessage(Message msg) {
-        try {
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "JMS payload receiced: {0}", msg.getJMSMessageID());
-        } catch (JMSException ex) {
-            Logger.getLogger(SampleWebSocket.class.getName()).log(Level.SEVERE, null, ex);
-        }
+    public void onClose(final Session session) {
+        System.out.println("onClose");
+        Logger.getLogger(SampleWebSocket.class.getName()).log(Level.INFO, "WebSocket session close()");
+        sessions.remove(session);
     }
 }
